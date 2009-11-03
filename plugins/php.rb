b07a75d34893b7e9e7eeb4919cbe7892e623217f
Juan Felipe Alvarez Saldarriaga <jfalvarez@vesifront.org>
2009-11-03 18:57:28 -0500

    Added !php_func command

--- plugins/php.rb
+++ plugins/php.rb
@@ -1,8 +1,8 @@
-#
-# Search on the php online documentation.
-#
 require "open-uri"
 
+#
+# Search on the online documentation
+#
 on :channel, /^!php (.+)/ do
   xhtml = Nokogiri::HTML( open( "http://www.php.net/results.php?q=#{match[0]}&p=manual&l=en" ) )
   xhtml.css( "#search-results li:first p.result a" ).each do | link |
@@ -12,5 +12,34 @@ on :channel, /^!php (.+)/ do
     end
 
     msg channel, "#{nick}: " + link.content + " - #{href}"
+  end
+end
+
+#
+# Display php function information
+#
+on :channel, /^!php_func (.+)/ do
+  func = match[0].gsub( /[_-]+/, "-" )
+
+  xhtml = Nokogiri::HTML( open( "http://www.php.net/manual/en/function.#{func}.php" ) )
+
+  ver = ""
+  xhtml.css( ".refnamediv p.verinfo" ).each do | version |
+    ver = version.content	
+  end
+
+  desc = ""
+  xhtml.css( ".refnamediv span.dc-title" ).each do | description |
+    desc = description.content	
+  end
+
+  syn = ""
+  xhtml.css( ".methodsynopsis" ).each do | synopsis |
+    syn = synopsis.content.gsub( /[\s]{2,}/, " " ).strip
+  end 
+
+  if ver != "" && desc != "" && syn != ""
+    msg channel, "#{ver} - #{desc}"
+    msg channel, "#{syn}"
   end
 end
