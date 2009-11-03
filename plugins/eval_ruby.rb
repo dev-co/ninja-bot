1b2d4a7f817ae3d0dfd3e94e891fbf3567a92cee
David A. Cuadrado <krawek@gmail.com>
2009-11-02 21:54:07 -0500

    plugin to eval ruby
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ plugins/eval_ruby.rb
@@ -0,0 +1,15 @@
+on :channel, /^!eval_ruby (.*)/ do
+  code = match.first
+  result = ""
+  Thread.start {
+    $SAFE = 4
+    begin
+    result = Object.module_eval(code)
+    rescue Exception
+      result = "you are so smart..."
+    end
+  }.join
+
+  msg channel, "#{nick}: #{result}"
+end
+
