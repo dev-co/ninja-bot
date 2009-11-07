3d459fa81fac413ae45e16de36f2271dd393f39b
Juan Felipe Alvarez Saldarriaga <jfalvarez@vesifront.org>
2009-11-07 10:20:14 -0500

    Added extra chars on the regex

--- plugins/parse_url.rb
+++ plugins/parse_url.rb
@@ -3,7 +3,7 @@ require "open-uri"
 #
 # Look if the message has a link and display his title.
 #
-on :channel, /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.-]*(\?\S+)?)?)?)/ do
+on :channel, /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.-\?\=\[\]&]*(\?\S+)?)?)?)/ do
   xhtml = Nokogiri::HTML( open( "#{match[0].strip}" ) )
   xhtml.xpath( "//title" ).each do | title |
     if ( title.content )
