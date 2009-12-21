e4d7ac1a0769203421cd7f8d75fc31450d9e367e
Juan Felipe Alvarez Saldarriaga <nebiros@Ubuntu.Belkin>
2009-12-20 20:22:21 -0500

    Added try/catch blocks, encode urls, etc.

--- plugins/parse_url.rb
+++ plugins/parse_url.rb
@@ -4,12 +4,15 @@ require "open-uri"
 # Look if the message has a link and display his title.
 #
 # See: http://flanders.co.nz/2009/11/08/a-good-url-regular-expression-repost/
-on :channel, /((?#Protocol)(http(?:s?)\:\/\/|~\/|\/)(?#Username:Password)(?:\w+:\w+@)?(?#Subdomains)(?:(?:[-\w]+\.)+(?#TopLevel Domains)(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|travel|[a-z]{2}))(?#Port)(?::[\d]{1,5})?(?#Directories)(?:(?:(?:\/(?:[-\w~!$+|.,:=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?#Query)(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?#Anchor)(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?)/ do
-p match[0].strip
-  xhtml = Nokogiri::HTML( open( "#{match[0].strip}" ) )
-  xhtml.xpath( "//title" ).each do | title |
-    if ( title.content )
-      msg channel, title.content
+on :channel, /((?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?(?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2})?)(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?)/ do
+  begin
+    xhtml = Nokogiri::HTML( open( "#{match[0].strip}" ) )
+    xhtml.xpath( "//title" ).each do | title |
+      if ( title.content )
+        msg channel, title.content.strip
+      end
     end
+  rescue Exception => e
+    p e
   end
 end
--- plugins/php.rb
+++ plugins/php.rb
@@ -1,17 +1,23 @@
 require "open-uri"
+require "cgi"
 
 #
 # Search on the online documentation
 #
 on :channel, /^!php (.+)/ do
-  xhtml = Nokogiri::HTML( open( "http://www.php.net/results.php?q=#{match[0]}&p=manual&l=en" ) )
-  xhtml.css( "#search-results li:first p.result a" ).each do | link |
-    href = link[:href]
-    if !href.match( /^http:\/\/(php.net|www.php.net)/i )
-      href = "http://www.php.net" + href
-    end
+  begin
+    query = CGI.escape( match[0].strip )
+    xhtml = Nokogiri::HTML( open( "http://www.php.net/results.php?q=#{query}&p=manual&l=en" ) )
+    xhtml.css( "#search-results li:first p.result a" ).each do | link |
+      href = link[:href]
+      if !href.match( /^http:\/\/(php.net|www.php.net)/i )
+        href = "http://www.php.net" + href
+      end
 
-    msg channel, "#{nick}: " + link.content.strip + " - #{href}"
+      msg channel, "#{nick}: " + link.content.strip + " - #{href}"
+    end
+  rescue Exception => e
+    p e
   end
 end
 
@@ -19,29 +25,32 @@ end
 # Display php function information
 #
 on :channel, /^!php_func (.+)/ do
-  func = match[0].gsub( /[_-]+/, "-" )
+  begin
+    func = match[0].gsub( /[_-]+/, "-" )
+    xhtml = Nokogiri::HTML( open( "http://www.php.net/manual/en/function.#{func.strip}.php" ) )
 
-  xhtml = Nokogiri::HTML( open( "http://www.php.net/manual/en/function.#{func}.php" ) )
+    ver = ""
+    xhtml.css( ".refnamediv p.verinfo" ).each do | version |
+      ver = version.content.strip
+    end
 
-  ver = ""
-  xhtml.css( ".refnamediv p.verinfo" ).each do | version |
-    ver = version.content.strip
-  end
+    desc = ""
+    xhtml.css( ".refnamediv span.dc-title" ).each do | description |
+      desc = description.content.strip
+    end
 
-  desc = ""
-  xhtml.css( ".refnamediv span.dc-title" ).each do | description |
-    desc = description.content.strip
-  end
+    syn = ""
+    xhtml.css( ".methodsynopsis" ).each do | synopsis |
+      syn = synopsis.content.gsub( /[\s]{2,}/, " " ).strip
+    end
 
-  syn = ""
-  xhtml.css( ".methodsynopsis" ).each do | synopsis |
-    syn = synopsis.content.gsub( /[\s]{2,}/, " " ).strip
-  end 
-
-  if ver != "" && desc != "" && syn != ""
-    msg channel, "#{ver} - #{desc}"
-    msg channel, "#{syn}"
-  else
-    msg channel, "Not Found"
+    if ver != "" && desc != "" && syn != ""
+      msg channel, "#{ver} - #{desc}"
+      msg channel, "#{syn}"
+    else
+      msg channel, "Not Found"
+    end
+  rescue Exception => e
+    p e
   end
 end
