828e2c77838aef542a383c1a949bce112b44fcf5
Juan Felipe Alvarez Saldarriaga <jfalvarez@vesifront.org>
2010-04-21 18:39:23 -0500

    * Changed xpath query.

--- plugins/parse_url.rb
+++ plugins/parse_url.rb
@@ -7,7 +7,7 @@ require "open-uri"
 on :channel, /((?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?(?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2})?)(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?)/ do
   begin
     xhtml = Nokogiri::HTML( open( "#{match[0].strip}" ) )
-    xhtml.xpath( "//title" ).each do | title |
+    xhtml.xpath( "//head/title" ).each do | title |
       if ( title.content )
         msg channel, title.content.gsub( /([\n\t])+{1,}/, " " ).strip
       end
