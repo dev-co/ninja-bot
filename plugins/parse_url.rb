1373aaaa0005cbbc686a711923fe4382c48c2da0
Andrés Felipe Vargas <andphe@gmail.com>
2010-05-11 23:14:58 -0500

    # parse_url plugin doesn't parse urls without using a prefix

--- plugins/parse_url.rb
+++ plugins/parse_url.rb
@@ -5,7 +5,7 @@ require "open-uri"
 #
 # See: http://flanders.co.nz/2009/11/08/a-good-url-regular-expression-repost/
 add_custom_pattern(:url, /((?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?(?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2})?)(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?)/)
-plugin ":text-url" do |m|
+plugin ":text-url", {:prefix => ''} do |m|
   begin
     xhtml = Nokogiri::HTML( open( "#{m.args[:text].strip}" ) )
     if m.args[:text].scan(/https?:\/\/twitter.com\/(\w+)\/status(es)?\/(\d+)/).empty?
