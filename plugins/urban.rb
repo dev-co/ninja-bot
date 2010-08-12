6da1687478605eb06bdb006667bc37ff9e232ede
David A. Cuadrado <krawek@gmail.com>
2010-08-12 11:17:10 -0500

    update urban plugin
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- plugins/urban.rb
+++ plugins/urban.rb
@@ -1,7 +1,7 @@
 module UrbanDictionary
   class Search
     include HTTParty
-    
+
     def initialize(term)
       @query = {}
       @query[:term] = "#{term}" if term && term.strip != ''
@@ -11,7 +11,7 @@ module UrbanDictionary
     def user_agent
       @user_agent = 'Ruby Twitter Gem'
     end
-    
+
     def fetch()
       response = self.class.get('http://www.urbandictionary.com/define.php', :query => @query, :headers => {'User-Agent' => user_agent})
     end
@@ -19,15 +19,12 @@ module UrbanDictionary
 end
 
 plugin "urban :text" do |m|
-  #begin
+  safe_run(m) do |m|
     xhtml = Nokogiri::HTML( UrbanDictionary::Search.new( m.args[:text] ).fetch )
     xhtml.xpath( '//table[@id="entries"]/tr[2]//div[@class="definition"]' ).each do | definition |
       if ( definition.content )
-	m.reply "#{m.nick}: #{definition.content.gsub( /([\n\t])+{1,}/, " " ).strip}"
+        m.reply "#{m.nick}: #{definition.content.gsub( /([\n\t])+{1,}/, " " ).strip}"
       end
     end
-
-  #rescue Exception => e
-  #  p e
-  #end    
+  end
 end
