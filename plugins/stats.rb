baf80a436272e74487e4c52654d0f72cb9292d46
David A. Cuadrado <krawek@gmail.com>
2010-08-13 12:49:38 -0500

    add new plugin to track stats
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ plugins/stats.rb
@@ -0,0 +1,76 @@
+require 'singleton'
+
+class UserDb
+  include Singleton
+
+  def initialize
+    @dbpath = File.expand_path("~/nbdb/")
+    FileUtils.mkpath(@dbpath)
+    puts "Initialized database in #{@dbpath}"
+  end
+
+  def parse(nick, chan, text)
+    if text =~ /\?/
+      store(nick, chan, "questions", text)
+    elsif text =~ /^\!/
+      store(nick, chan, "commands", text)
+    elsif text =~ /fuck|fu|mofo|put(a|o)|mierda|shit|malpar|hijue|hijo\sde\sputa/i
+      store(nick, chan, "badwords", text)
+    end
+  end
+
+  def update_seen(nick, message = "")
+    store(nick, "", "seen", "#{Time.now.to_i} #{message}", true)
+  end
+
+  def last_seen(nick)
+    path = File.join(userdb_path(nick, ""), "seen")
+
+    if File.exist?(path)
+      time, message = File.read(path).split(" ", 2)
+      [Time.at(time.to_i).ago, message]
+    else
+      nil
+    end
+  end
+
+  protected
+  def store(nick, chan, scope, data, clear = false)
+    mode = "a"
+    model = "w" if clear
+
+    File.open(File.join(userdb_path(nick,chan),scope), mode) do |f|
+      f.puts data
+    end
+  end
+
+  def userdb_path(nick, chan)
+    userdb_path = File.join(@dbpath,chan.to_s.downcase, nick.to_s.downcase)
+    FileUtils.mkpath(userdb_path)
+    userdb_path
+  end
+end
+
+on :quit do |m|
+  UserDb.instance.update_seen(m.nick, m.text)
+end
+
+plugin ":channel_message", :prefix => "" do |m|
+  safe_run(m, m.args) do |m, args|
+    UserDb.instance.parse(m.nick, m.chan, args[:channel_message])
+  end
+end
+
+plugin "seen :target" do |m|
+  safe_run(m, m.args) do |m, args|
+    if (channel_names[m.channel] || []).include?(args[:target])
+      m.reply "#{m.nick}: you need new glasses"
+    elsif seen = UserDb.instance.last_seen(args[:target])
+      m.reply "#{m.nick}: Last time I saw #{args[:target]} was #{seen.first}. it said: #{seen.last}"
+    elsif m.nick == args[:target]
+      m.reply "#{m.nick}: you need a new brain"
+    else
+      m.reply "I've never seen that guy #{args[:target]} over here."
+    end
+  end
+end
