46f1828a63a7a96dabfe83921c62ffebb46e7004
Guillermo <guilleiguaran@gmail.com>
2010-08-15 10:22:12 -0500

    Updating github, gem plugins

--- plugins/gem.rb
+++ plugins/gem.rb
@@ -2,13 +2,15 @@ require 'json'
 require 'open-uri'
 
 plugin "gem :text" do |m|
-    url = "http://rubygems.org/api/v1/gems/#{URI.escape(m.args[:text])}.json"
-    begin
-      result = JSON.parse(open(url).read)
-      reply = "#{result['name']} (by #{result['authors']}): #{result['project_uri']}"
-    rescue
-      reply = "I don't know to #{text} gem :/"
-    end
+    safe_run(m, m.args) do |m, args|
+      url = "http://rubygems.org/api/v1/gems/#{URI.escape(args[:text])}.json"
+      begin
+        result = JSON.parse(open(url).read)
+        reply = "#{result['name']} (by #{result['authors']}): #{result['project_uri']}"
+      rescue
+        reply = "I don't know to #{args[:text]} gem :/"
+      end
   
-    m.reply "#{m.nick}: #{reply}"
+      m.reply "#{m.nick}: #{reply}"
+    end
 end
--- plugins/github.rb
+++ plugins/github.rb
@@ -2,15 +2,16 @@ require 'json'
 require 'open-uri'
 
 plugin "github :text" do |m|
-    text = m.args[:text]
-    url = "http://github.com/api/v1/json/search/#{URI.escape(text)}"
-    result = JSON.parse(open(url).read)
-    if result["repositories"].empty?
-      reply = "I don't know to #{text} :|"
-    else
-      project = result["repositories"][0]
-      reply = "#{project['name']}: http://github.com/#{project['username']}/#{project['name']}"
-    end
+    safe_run(m, m.args) do |m, args|
+      url = "http://github.com/api/v1/json/search/#{URI.escape(args[:text])}"
+      result = JSON.parse(open(url).read)
+      if result["repositories"].empty?
+        reply = "I don't know to #{args[:text]} :|"
+      else
+        project = result["repositories"][0]
+        reply = "#{project['name']}: http://github.com/#{project['username']}/#{project['name']}"
+      end
   
-    m.reply "#{m.nick}: #{reply}"
+      m.reply "#{m.nick}: #{reply}"
+    end
 end
