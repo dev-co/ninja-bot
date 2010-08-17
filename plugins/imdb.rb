962170464b8f320e66cbf395cb2d424420618015
David A. Cuadrado <krawek@gmail.com>
2010-08-17 18:35:33 -0500

    do not flood the channel
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- plugins/imdb.rb
+++ plugins/imdb.rb
@@ -3,8 +3,12 @@ require 'imdb'
 plugin "imdb :text" do |m|
   safe_run(m, m.args) do |m, args|
     movies = Imdb::Search.new(args[:text]).movies
-    movies[0,3].each do |movie|
-	    m.reply "#{m.nick}: #{movie.title}[#{movie.year}] #{movie.plot[0,50]}.. #{movie.rating}/10 #{movie.url}"
+    movies[0,5].each_with_index do |movie, index|
+      if index == 0
+	      m.reply "#{m.nick}: #{movie.title}[#{movie.year}] #{movie.plot.to_s[0,50]}.. #{movie.rating}/10 #{movie.url}"
+      else
+	      m.irc.privmsg m.nick, "#{movie.title}[#{movie.year}] #{movie.plot.to_s[0,50]}.. #{movie.rating}/10 #{movie.url}"
+      end
     end
   end
 end
