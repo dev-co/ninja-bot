b03b1acef3c51ca95c7f3852ba6647d774ecede0
David A. Cuadrado <krawek@gmail.com>
2010-08-24 17:47:01 -0500

    improve rubydoc plugin
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- plugins/rubydoc.rb
+++ plugins/rubydoc.rb
@@ -4,16 +4,38 @@ class RubyDoc
   def execute(bot, query)
     count = 0
 
-    baseurl = "http://ruby-doc.org/ruby-1.9"
-    doc = Nokogiri::HTML(open("#{baseurl}/fr_class_index.html"))
+    base_url = "http://ruby-doc.org/ruby-1.9"
+    klass, method = query.split(" ", 2)
+
+    agent = Mechanize.new
+    page = agent.get("#{base_url}/fr_class_index.html")
+    klass_link = page.link_with(:text => /^\s*#{Regexp.escape(klass)}\s*$/i )
 
     result = "not found"
-    doc.css("a").each do |link|
-      if link.text.strip.downcase == query.downcase
-        result = "#{link.text} #{baseurl}/#{link["href"]}"
-        break
+    if klass_link
+      url = "#{base_url}/#{klass_link.uri.to_s}"
+
+      found = false
+      if method
+        page = klass_link.click
+        if method_link = page.link_with(:text => /^\s*#{Regexp.escape(method)}\s*$/i)
+          ref = method_link.node["href"].sub("#", "")
+          section = page.parser.css("#method-#{ref}").first
+          if section
+            found = true
+            signature = section.css(".method-signature").first.content.strip.gsub(/\s+/, " ")
+            description = section.css(".method-description p").first.content[0,250].strip.gsub(/\s+/, " ")
+
+            result = "#{signature}: #{description}.. #{url}##{ref}"
+          end
+        end
+      end
+
+      if !found
+        result = "#{klass_link.text} #{url}"
       end
     end
+
     bot.reply "#{bot.nick}: #{result}"
   end
 end
