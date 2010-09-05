f0947e556bb176426fed06498734f7d8334b8b9d
David A. Cuadrado <krawek@gmail.com>
2010-09-05 16:45:04 -0500

    move parse url to NinjaPlugin module
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/ninja_plugin.rb
+++ lib/ninja_plugin.rb
@@ -11,4 +11,24 @@ module NinjaPlugin
   def shorten_url(url)
     open("http://bit.ly/api?url=#{url}").read rescue url
   end
+
+  def parse_url(url)
+    xhtml = Nokogiri::HTML(open(url))
+    content = ""
+    if /https?:\/\/([a-z]*\.)?twitter.com\/(\w+)\/status(es)?\/(\d+)/ =~ url
+      xhtml.xpath('//span[@class="entry-content"]').each do |tweet|
+        content = tweet.content.gsub(/([\n\t])+{1,}/, " ").strip if tweet.content
+      end
+    elsif /https?:\/\/identi.ca\/notice\/(\d+)/ =~ url
+      xhtml.xpath('//p[@class="entry-content"]').each do |tweet|
+        content = tweet.content.gsub(/([\n\t])+{1,}/, " ").strip if tweet.content
+      end
+    else
+      xhtml.xpath("//head/title").each do |title|
+        content = title.content.gsub(/([\n\t])+{1,}/, " ").strip if title.content
+      end
+    end
+
+    content
+  end
 end
