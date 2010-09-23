d8feaabd4efd462c1d618382dd4a28d2375a08a6
David A. Cuadrado <krawek@gmail.com>
2010-09-22 21:47:37 -0500

    improve karma plugin
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -7,7 +7,7 @@ class KarmaPlugin
 
   match /karma (.+)/, :method => :stats
   match /fans (.+)/, :method => :fans
-  match /\b(\S+)\s*(\+\+|\-\-|\+1)(\s|$)/, :use_prefix => false
+  match /\b(\S+)\s*(\+\+|\-\-|\+1|lol|thanks|thx|gracias)(\s|$)/, :use_prefix => false
 
   def stats(m, nick)
     if m.channel && (target = Channel.get_user(m.channel.name, nick, false))
@@ -29,7 +29,7 @@ class KarmaPlugin
     return if m.channel.nil?
 
     synchronize(:karma) do
-      nick.sub!(":", "")
+      nick.gsub!(/:|,/, "")
       nick.downcase!
 
       rnick = m.user.nick
@@ -50,16 +50,16 @@ class KarmaPlugin
         target = Channel.get_user(m.channel.name, irc_user.nick)
         source = Channel.get_user(m.channel.name, rnick)
 
-        if (oper == "++" || oper == "+1") && source.can_increase_karma?
+        if (["++", "+1", "lol", "thx", "thanks", "gracias"].include?(oper)) && source.can_increase_karma?
           target.karma_up!
           target.add_fan(source.nick)
           source.given_points_up!
           irc_user.send("Your karma has been increased by #{rnick}. currently you have [#{target.karma_up+1} - #{target.karma_down} == #{target.karma+1}] points of karma.")
-          m.user.send("You have given 1 point of karma to #{irc_user.nick}. you have given #{source.given_points_today+1} points today")
+          m.user.send("You have given 1 point of karma to #{irc_user.nick}. you have given #{source.given_points_today} points today")
         elsif oper == "--" && source.can_decrease_karma?
           target.karma_down!
           source.given_points_up!
-          m.user.send("You have taken 1 point of karma from #{irc_user.nick}. you have given #{source.given_points_today+1} points today")
+          m.user.send("You have taken 1 point of karma from #{irc_user.nick}. you have given #{source.given_points_today} points today")
         end
       end
     end
