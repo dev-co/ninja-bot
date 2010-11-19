b22435adc193ecf214fdc5f172cfa8c1a6b2c6c1
David A. Cuadrado <krawek@gmail.com>
2010-11-19 14:16:23 -0500

    fix gemdoc plugin
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- plugins/gemdoc.rb
+++ plugins/gemdoc.rb
@@ -8,11 +8,9 @@ class GemDocPlugin
   end
 
   def execute(m, query)
-    xhtml = Nokogiri::HTML(open("http://rdoc.info/projects/search?q=#{URI.escape(query)}"))
-    links = xhtml.css(".projects .project a")[0,10]
-    result = links.map do |link|
-      "http://rdoc.info#{link[:href]}"
-    end.join(" -- ")
+    xhtml = Nokogiri::HTML(open("http://rdoc.info/find/gems?q=#{URI.escape(query)}"))
+    link = xhtml.css("ul.libraries li a:first").first
+    result = "http://rdoc.info#{link[:href].to_s.gsub(/^#/, "")}"
     m.reply "#{m.user.nick}: #{result}"
   end
 end
