a6ab705cd56c5a136f4703464e145e323647fe31
julianduque <julianduquej@gmail.com>
2011-07-13 20:59:33 -0500

    Added currency formating to usd2cop plugin

--- plugins/usd_to_cop.rb
+++ plugins/usd_to_cop.rb
@@ -15,7 +15,7 @@ class UsdToCopPlugin
     begin
       doc = Nokogiri::HTML(open("http://www.colombia.com/colombiainfo/estadisticas/dolar.asp"))
       cop = doc.css('.trm').last.children.to_s.gsub(',','').to_f
-      reply = cop * query.to_f
+      reply = currencify(cop * query.to_f)
     rescue
       reply = "Error reading exchange rate :|"
     end
@@ -25,6 +25,27 @@ class UsdToCopPlugin
   def trm(m)
     convert(m, 1)
   end
+
+  # As seen in: http://codesnippets.joyent.com/posts/show/1812 
+  # takes a number and options hash and outputs a string in any currency format
+  def currencify(number, options={})
+    # :currency_before => false puts the currency symbol after the number
+    # default format: $12,345,678.90
+    options = {:currency_symbol => "$", :delimiter => ",", :decimal_symbol => ".", :currency_before => true}.merge(options)
+          
+    # split integer and fractional parts 
+    int, frac = ("%.2f" % number).split('.')
+    # insert the delimiters
+    int.gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{options[:delimiter]}")
+                     
+    if options[:currency_before]
+      options[:currency_symbol] + int + options[:decimal_symbol] + frac
+    else
+      int + options[:decimal_symbol] + frac + options[:currency_symbol]
+    end
+
+  end
+
 end
 
 register_plugin UsdToCopPlugin
