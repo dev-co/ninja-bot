65c80babee628d0dc4d7cc92069125d95f302747
Carlos León <mail@carlosleon.info>
2012-05-22 14:48:25 -0500

    Fixing security problem with 'eval_ruby' plugin.

--- plugins/eval_ruby.rb
+++ plugins/eval_ruby.rb
@@ -1,5 +1,6 @@
 class EvalRubyPlugin
   include NinjaPlugin
+  BLACKLIST = %w[ Thread Dir File FileUtils ]
 
   match /ruby (.+)/
 
@@ -10,21 +11,38 @@ class EvalRubyPlugin
   def execute(m, code)
     result = ""
 
-    Timeout.timeout(2) do
-      Thread.start {
-        $SAFE = 4
-        begin
-          result = Object.module_eval(code).inspect
-        rescue SecurityError
-          result = "nah nah, you suck man"
-        rescue Exception => e
-          result = "you are so smart... #{e.to_s}"
-        end
-      }.join
+    if is_unsafe? code
+      result = "don't try to hack me, you fucker!"
+    else
+      Timeout.timeout(2) do
+        Thread.start {
+          $SAFE = 4
+          begin
+            result = Object.module_eval(code).inspect
+          rescue SecurityError
+            result = "nah nah, you suck man"
+          rescue Exception => e
+            result = "you are so smart... #{e.to_s}"
+          end
+        }.join
+      end
     end
 
     m.reply "#{m.user.nick}: #{result[0,200]}"
   end
+
+  protected
+
+    def is_unsafe?(code)
+      code =~ /(Thread|Dir|File|FileUtils)\./
+    end
+
+    # This will return a regular expression that looks like this:
+    # /(Thread|Dir|File|FileUtils)/
+    def blacklist
+      Regexp.new "(#{ BLACKLIST.join '|' })"
+    end
+
 end
 
 register_plugin EvalRubyPlugin
