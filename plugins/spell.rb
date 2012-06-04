cc7215fc5b6f5f9a69df117bbf3196eaefcff334
Carlos León <mail@carlosleon.info>
2012-06-04 10:50:21 -0500

    Adding Spell plugin.

---
+++ plugins/spell.rb
@@ -0,0 +1,15 @@
+class SpellPlugin
+  include NinjaPlugin
+
+  match /spell (.+)/
+
+  def usage
+    '!spell <word> -- spell a word'
+  end
+
+  def execute(bot, word)
+    word = word.upcase.split(//).join ' '
+    bot.reply word
+  end
+end
+register_plugin SpellPlugin
